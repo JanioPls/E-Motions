029df63867cc5e254741a8ac6ec2641843ffb594
Jacob Manio <80988292+JanioPls@users.noreply.github.com>
2021-04-09 14:28:36 -0700

    Add files via upload

---
+++ src/stacks/MainStackScreen.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import { Entypo, AntDesign } from '@expo/vector-icons';
+
+
+
+import HomeScreen from '../screens/HomeScreen';
+import EntryScreen from '../screens/EntryScreen';
+import StatisticScreen from '../screens/StatisticScreen';
+import CalendarScreen from '../screens/CalendarScreen';
+import SettingScreen from '../screens/SettingScreen';
+
+
+export default MainStackScreens = () => {
+    const MainStack = createBottomTabNavigator()
+
+    const tabBarOptions = {
+        showLabel: false,
+        style: {
+            backgroundColor: "#FCF8EC",
+            paddingBottom: 2
+        },
+    };
+
+    const screenOptions = ({route}) => ({
+        tabBarIcon: ({focused}) => {
+
+            switch (route.name)
+            {
+                case "Home":
+                    return <Entypo name="home" size={24} color={focused ? "#D0E8F2" : "#666666"} />
+                    break;
+
+                case "Entry":
+                    return <Entypo name="clipboard" size={24} color={focused ? "#D0E8F2" : "#666666"} />
+                    break;
+
+                case "Stat":
+                    return <Entypo name="bar-graph" size={24} color={focused ? "#D0E8F2" : "#666666"} />
+                    break;
+
+                case "Calendar":
+                    return <Entypo name="calendar" size={24} color={focused ? "#D0E8F2" : "#666666"} />
+                    break;
+
+                case "Setting":
+                    return <AntDesign name="setting" size={24} color={focused ? "#D0E8F2" : "#666666"} />   
+       
+            }
+
+            
+        },
+    });
+
+    return(
+        <MainStack.Navigator tabBarOptions={tabBarOptions} screenOptions = {screenOptions}>
+            <MainStack.Screen name ="Home" component={HomeScreen}/>
+            <MainStack.Screen name ="Entry" component={EntryScreen}/>
+            <MainStack.Screen name ="Stat" component={StatisticScreen}/>
+            <MainStack.Screen name ="Calendar" component={CalendarScreen}/>
+            <MainStack.Screen name ="Setting" component={SettingScreen}/>
+        </MainStack.Navigator>
+    );
+};
